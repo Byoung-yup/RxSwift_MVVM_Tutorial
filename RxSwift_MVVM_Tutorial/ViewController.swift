3ed9e718320ec8a8c2a283be5a1659c99edbfcd0
Byoung-yup <20170926@vision.hoseo.edu>
2022-11-11 22:21:02 +0900

    tableview implement add/delete/edit

--- RxSwift_MVVM_Tutorial/ViewController.swift
+++ RxSwift_MVVM_Tutorial/ViewController.swift
@@ -22,10 +22,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureNavigationTitle()
+        let addBtn = UIBarButtonItem(title: "Add", style: .done, target: self, action: #selector(addTap))
+        self.navigationItem.rightBarButtonItem = addBtn
         view.addSubview(tableView)
         viewModel.fetchUsers()
         bindTableView()
+    }
+    
+    func configureNavigationTitle(){
+        self.title = "Users"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Copperplate", size: 21) ?? UIFont()]
+    }
+    
+    @objc func addTap() {
+        let user = User(userID: 48954, id: 4534, title: "CodeLib", body: "RxSwift Crud")
+        self.viewModel.addUser(user: user)
     }
 
     func bindTableView() {
@@ -33,6 +45,24 @@ class ViewController: UIViewController {
         viewModel.users.bind(to: tableView.rx.items(cellIdentifier: UserTableViewCell.identifier)) { (row, item, cell) in
             cell.textLabel?.text = item.title
             cell.detailTextLabel?.text = "\(item.id)"
+            cell.selectionStyle = .none
+        }.disposed(by: bag)
+        
+        tableView.rx.itemSelected.subscribe { indexPath in
+            let alert = UIAlertController(title: "Node", message: "Edit", preferredStyle: .alert)
+            alert.addTextField()
+            alert.addAction(UIAlertAction(title: "Edit", style: .default) { _ in
+                let textField = alert.textFields![0] as UITextField
+                self.viewModel.editUser(title: textField.text!, index: indexPath.row)
+            })
+            DispatchQueue.main.async {
+                self.present(alert, animated: false)
+            }
+        }.disposed(by: bag)
+        
+        tableView.rx.itemDeleted.subscribe { [weak self] indexPath in
+            guard let self = self else { return }
+            self.viewModel.deleteUser(index: indexPath.row)
         }.disposed(by: bag)
     }
 }
